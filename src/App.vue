813c06d2c15b23a5b705bcabf2aaa9509e4d132c
neider_camargo <nfcamargo99@outlook.com>
2023-10-28 20:11:57 -0500

    metodo common para mostrar el nombre del país

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import pageHeader from './components/pageHeader.vue';
 import axiosClient from "./utils/axios";
-import { onMounted } from 'vue';
+import { onMounted, ref} from 'vue';
+import {Country} from "./models/country.model";
+
+//variable de estado
+const countries=ref<Country[]>([]);//constante que guarda un estado de los países
 
 //función para consultar los paises
 const fetchCountries = async ()=>{
-  const {data} = await axiosClient.get("/all")
-  
+  try{
+    const {data} = await axiosClient.get("/all")
+    countries.value=data;
+  }catch(error){//mensaje por si hay algun error
+    console.log(error)
+  }
 };
 
 //ejecutar función
@@ -19,4 +27,5 @@ onMounted(()=>{
 <template>
   <!--Con esto llamamos al template del archico pageheader-->
   <pageHeader/>
+  <div v-for="country in countries">{{ country.name.common}}</div>
 </template>
