f4ce627e02748279c94e9df4bea325aea362ecde
neider_camargo <nfcamargo99@outlook.com>
2023-10-28 21:25:24 -0500

    creación de las vajas de cada país

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import pageHeader from './components/pageHeader.vue';
 import axiosClient from "./utils/axios";
 import { onMounted, ref} from 'vue';
 import {Country} from "./models/country.model";
+import CountryList from "./components/CountryList.vue";
 
 //variable de estado
 const countries=ref<Country[]>([]);//constante que guarda un estado de los países
@@ -27,5 +28,8 @@ onMounted(()=>{
 <template>
   <!--Con esto llamamos al template del archico pageheader-->
   <pageHeader/>
-  <div v-for="country in countries">{{ country.name.common}}</div>
+  <div class="container max-w-screen-lg mx-auto px-6">
+    <CountryList :countries="countries"/>
+  </div>
+  
 </template>
---
+++ src/components/CountryList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {defineProps} from "vue";
+
+interface Props{
+    countries: Country[];   
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <!--Forma en que se muestra las tarjetas-->
+    <div class="grid grid-cols-1" sm:grid-2 md:grid-cols-3 gap3></div>
+    <div v-for="country in countries" class="border border-gray-300 rounded p-3">{{ country.name.common }}</div>
+</template>
