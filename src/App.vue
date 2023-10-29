39e582095728a5bd8c961a5cb89715dd57374143
neider_camargo <nfcamargo99@outlook.com>
2023-10-28 19:20:06 -0500

    conexión  a la api de los países  restcountries

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
+import pageHeader from './components/pageHeader.vue';
+import axiosClient from "./utils/axios";
+import { onMounted } from 'vue';
 
+//función para consultar los paises
+const fetchCountries = async ()=>{
+  const {data} = await axiosClient.get("/all")
+  
+};
+
+//ejecutar función
+fetchCountries();
+onMounted(()=>{
+  fetchCountries();
+})
 </script>
 
 <template>
-  <h1>countries-app</h1>
-  <h1 class="text-3xl font-bold underline">
-    countries-app
-  </h1>  
+  <!--Con esto llamamos al template del archico pageheader-->
+  <pageHeader/>
 </template>
---
+++ src/components/pageHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center my-24">
+        <h1 class="text-4xl fond-bold tracking-widest text-sky-700">Countries app</h1>
+        <h6 class="mt-4 text-xl">app para buscar paises</h6>
+    </div>
+</template>
